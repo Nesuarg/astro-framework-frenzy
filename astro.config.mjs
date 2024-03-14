ecb4ac6811019e9c430fde2f4945fbef062a73de
Jannik FIscher <jgf_ext@sport24.dk>
2024-03-14 15:05:05 +0100

    install sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), lit(), svelte()]
+  integrations: [tailwind(), lit(), svelte()],
+  
 });
