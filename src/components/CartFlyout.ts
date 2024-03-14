14bc3fc406320c5ff6482cbb8eca61490710d124
Jannik FIscher <jgf_ext@sport24.dk>
2024-03-14 14:30:52 +0100

    bump

--- src/components/CartFlyout.ts
+++ src/components/CartFlyout.ts
@@ -1,16 +1,37 @@
 import { LitElement, html } from "lit";
 import { customElement } from "lit/decorators.js";
 import { StoreController } from "@nanostores/lit";
-import { isCartOpen } from '../cartStore'
+import { isCartOpen, cartItems } from '../cartStore'
 
 @customElement('cart-flyout')
 export class CartFlyout extends LitElement {
 
   private cartOpen = new StoreController(this, isCartOpen);
+  private getCartItems = new StoreController(this, cartItems);
+
+  renderCartItem(cartItem) {
+    return html`
+      <li>
+        <img src="${cartItem.imageSrc}" alt="${cartItem.name}" />
+        <h3>${cartItem.name}</h3>
+        <p>Quantity: ${cartItem.quantity}</p>
+      </li>
+    `;
+  }
 
   render() {
     return this.cartOpen.value ? html`<aside>
-      cart
-    </aside>` : null;
+    ${
+      Object.values(this.getCartItems.value).length
+        ? html`
+          <ul>
+            ${Object.values(this.getCartItems.value).map((cartItem) =>
+              this.renderCartItem(cartItem)
+            )}
+          </ul>
+        `
+        : html`<p>Your cart is empty!</p>`
+    }
+  </aside>` : null;
   }
 }
